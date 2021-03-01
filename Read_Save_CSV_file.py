2eae6bca2cbf7a568799eac0f4541d5419d2b00a
Mingming Zhang <mzhang607@gatech.edu>
2021-03-01 10:17:31 -0500

    update read csv file

--- Read_Save_CSV_file.py
+++ Read_Save_CSV_file.py
@@ -6,7 +6,14 @@ Created on Thu Dec 17 13:35:34 2020
 """
 
 import csv
+import pandas as pd
 # open a new CSV file to write data
+
+
+##########################################################################
+########## Example of opening a file, and write data into it   ###########
+##########################################################################
+myfilename = "myfile.csv"
 
 csvfile  = open("myfile.csv",'w', newline='')   # create a file with the name you want
 datawriter = csv.writer(csvfile)       # create a writer object
@@ -23,3 +30,24 @@ data_to_write = (1,  123, -158, 10.6)
 datawriter.writerow(data_to_write) 
 
 csvfile.close() # close the .csv file after finishing save the data
+
+
+##########################################################################
+########## Example of opening a file, and read data from it    ###########
+##########################################################################
+
+df = pd.read_csv(myfilename) # load the data in as pandas data frame
+data_value  = df.values      # get all data values
+keys_value  = df.keys        # get all the keys and data values
+keys  = df.keys()            # get all the keys
+
+# find if a given substring is in my current string or not
+"""
+if this substring is not in my string, it will return -1
+"""
+
+(myfilename.find("findMyString") != -1) 
+
+
+
+
